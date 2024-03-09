cc7683d4d168747c0ebd1b7ddd78fd326cb75b8c
prahlad <kamatprahlad43@gmail.com>
2024-03-09 20:00:57 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Modern_Webpage/",
+  base: "/Awwwarded-Website-Clone/",
   server: {
     port: 3000,
 
